b4167e06c2603323f88c543db98667fb7772442f
kemo-sabe <kemo-sabe@users.noreply.github.com>
2019-11-07 12:20:55 -0600

    Create pageOrder.js

---
+++ scripts/pageOrder.js
@@ -0,0 +1,568 @@
+// JavaScript Document
+
+var pageOrder = {
+	"mid" :
+		[
+			"01_unit/",
+			"02_unit/",
+			"03_unit/",
+		],
+	"u1" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							"00_unit_courseIntro",
+							"01_lesson",
+							"02_lesson",
+							"03_lesson",
+							"04_lesson",
+							"05_lesson",
+						]
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						]
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+						]
+					]
+			}
+		],
+	"u2a" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							// Main
+							"00_unit_courseIntro",
+							"01_lesson1Intro",
+							"02_lesson2Intro",
+							"03_lesson3Intro",
+							"04_lesson4Intro",
+						],
+						[
+							// Lesson 1
+							"01_topic",
+							"01_topic",
+							"02_topic",
+							"03_topic",
+							"04_topic",
+							"05_topic",
+							"06_topic",
+							"07_topic",
+						],
+						[
+							// Lesson 2
+							"02_lesson2Intro",
+							"01_topic",
+							"02_topic",
+							"03_topic",
+						],
+						[
+							// Lesson 3
+							"03_lesson3Intro",
+							"01_topic",
+							"02_topic",
+							"03_topic",
+							"04_topic",
+							"05_topic",
+							"06_topic",
+							"07_topic",
+							"08_topic",
+							"09_topic",
+							"10_topic",
+							"11_topic",
+						],
+						[
+							// Lesson 4
+							"04_lesson4Intro",
+							"01_topic",
+							"02_topic",
+							"03_topic",
+							"04_topic",
+							"05_topic",
+						],
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							// Main
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 1
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 2
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 3
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 4
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							// Main
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 1
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 2
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 3
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 4
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+							"Title",
+						],
+					]
+			}
+		],
+	"u2b" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							// Main 
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+					]
+			}
+		],
+	"u3a" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							// Main
+							"00_dutyIntro",
+							"lesson1Intro",
+							"lesson2Intro",
+							"lesson3Intro",
+						],
+						[
+							// Lesson 1
+							"lesson1Intro",
+							"01_topic",
+							"02_topic",
+						],
+						[
+							// Lesson 2
+							"lesson2Intro",
+							"01_topic",
+							"02_topic",
+							"03_topic",
+						],
+						[
+							// Lesson 3
+							"lesson3Intro",
+							"01_topic",
+							"02_topic",
+						],
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							// Main
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 1
+							"Introduction",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 2
+							"Introduction",
+							"Main Title",
+							"Main Title",
+							"Main Title",
+						],
+						[
+							// Lesson 3
+							"Introduction",
+							"Main Title",
+							"Main Title",
+						],
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							// Main
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 1
+							"Introduction",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 2
+							"Introduction",
+							"Title",
+							"Title",
+							"Title",
+						],
+						[
+							// Lesson 3
+							"Introduction",
+							"Title",
+							"Title",
+						],
+					]
+			}
+		],
+	"u3b" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+					]
+			}
+		],
+	"u4" :
+		[
+			{
+				"pageOrder" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+						[
+							// Lesson 6
+						],
+						[
+							// Lesson 7
+						],
+						[
+							// Lesson 8
+						],
+						[
+							// Lesson 9
+						],
+						[
+							// Lesson 10
+						],
+					]
+			},
+			{
+				"title" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+						[
+							// Lesson 6
+						],
+						[
+							// Lesson 7
+						],
+						[
+							// Lesson 8
+						],
+						[
+							// Lesson 9
+						],
+						[
+							// Lesson 10
+						],
+					]
+			},
+			{
+				"sideTitle" :
+					[
+						[
+							// Main
+						],
+						[
+							// Lesson 1
+						],
+						[
+							// Lesson 2
+						],
+						[
+							// Lesson 3
+						],
+						[
+							// Lesson 4
+						],
+						[
+							// Lesson 5
+						],
+						[
+							// Lesson 6
+						],
+						[
+							// Lesson 7
+						],
+						[
+							// Lesson 8
+						],
+						[
+							// Lesson 9
+						],
+						[
+							// Lesson 10
+						],
+					]
+			}
+		],
+};
